8d481749900c78f4045e90f9d3f665e2e68f2cf3
ruchajagatkumarshah <rjshah33.rs@gmail.com>
2020-01-23 19:21:58 +0530

    cart, order and checkout improvements

---
+++ app/src/main/java/com/project/sportycart/entity/CartDetailsDTO.java
@@ -0,0 +1,61 @@
+package com.project.sportycart.entity;
+
+public class CartDetailsDTO{
+	private int quantity;
+	private String productId;
+	private String merchantId;
+	private double price;
+	private String userId;
+
+	public void setQuantity(int quantity){
+		this.quantity = quantity;
+	}
+
+	public int getQuantity(){
+		return quantity;
+	}
+
+	public void setProductId(String productId){
+		this.productId = productId;
+	}
+
+	public String getProductId(){
+		return productId;
+	}
+
+	public void setMerchantId(String merchantId){
+		this.merchantId = merchantId;
+	}
+
+	public String getMerchantId(){
+		return merchantId;
+	}
+
+	public void setPrice(double price){
+		this.price = price;
+	}
+
+	public double getPrice(){
+		return price;
+	}
+
+	public void setUserId(String userId){
+		this.userId = userId;
+	}
+
+	public String getUserId(){
+		return userId;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"CartDetailsDTO{" + 
+			"quantity = '" + quantity + '\'' + 
+			",productId = '" + productId + '\'' + 
+			",merchantId = '" + merchantId + '\'' + 
+			",price = '" + price + '\'' + 
+			",userId = '" + userId + '\'' + 
+			"}";
+		}
+}
