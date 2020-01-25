87e4201d7bcc8280317090d5be552ec0fd7ac71d
ruchajagatkumarshah <rjshah33.rs@gmail.com>
2020-01-25 12:34:34 +0530

    finalBranch day 7

--- app/src/main/java/com/project/sportycart/OrderLog.java
+++ app/src/main/java/com/project/sportycart/OrderLog.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import com.project.sportycart.adapter.OrderLogAdapter;
 import com.project.sportycart.entity.OrderTable;
 import com.project.sportycart.entity.RegisterUser;
+import com.project.sportycart.retrofit.GetOrderApis;
 import com.project.sportycart.retrofit.GetProductsService;
 import com.project.sportycart.retrofit.RetrofitClientInstance;
 import java.util.List;
@@ -19,6 +20,7 @@ public class OrderLog extends AppCompatActivity {
     private RecyclerView.Adapter orderLogAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private RegisterUser registerUser;
+    List<OrderTable> orderTableList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +29,20 @@ public class OrderLog extends AppCompatActivity {
         layoutManager = new GridLayoutManager(this,1);
         recyclerView = (RecyclerView)findViewById(R.id.recyle_view);
         recyclerView.setLayoutManager(layoutManager);
-        GetProductsService getProductsService = RetrofitClientInstance.getRetrofitInstance().create(GetProductsService.class);
-        getProductsService.getOrderLog(registerUser).enqueue(new Callback<List<OrderTable>>() {
+
+
+        GetOrderApis getOrderApis=RetrofitClientInstance.getRetrofitInstance().create(GetOrderApis.class);
+
+        //getOrderApis.getRecentOrders(userId).enqueue(new Callback<List<OrderTable>>() {
+
+        getOrderApis.getRecentOrders("75").enqueue(new Callback<List<OrderTable>>() {
             @Override
             public void onResponse(Call<List<OrderTable>> call, Response<List<OrderTable>> response) {
-                List<OrderTable> list = response.body();
-                orderLogAdapter  = new OrderLogAdapter(list);
-                recyclerView.setAdapter(orderLogAdapter);
+                orderTableList=response.body();
+                for(OrderTable orderTable: orderTableList)
+                {
+                    OrderCollection.add(orderTable);
+                }
             }
 
             @Override
@@ -43,3 +52,6 @@ public class OrderLog extends AppCompatActivity {
         });
     }
 }
+
+//373683242655-0f1t86uv6ntj57dipepnr9tsdcs7kjll.apps.googleusercontent.com
+// deTR-mhHCoSRtb5wqehSQpVu
